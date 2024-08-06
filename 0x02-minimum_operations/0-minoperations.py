31e50c77853f5a4bd94769852a3ec750984cfd38
KARAM022 <my0fav0library@gmail.com>
2024-08-06 15:07:37 +0100

    alx

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-""" Min operations """
+"""
+minOperations
+"""
 
-def minOperations(n: int) -> int:
-    """ Min operations """
+def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to reach a target number of H characters.
+    
+    Args:
+        n (int): H characters.
+    
+    Returns:
+        int: The fewest number of operations needed, or 0 if impossible.
+    """
     if n <= 1:
         return 0
-    for i in range(2, n + 1):
-        if n % i == 0:
-            return minOperations(n // i) + i
-    return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
